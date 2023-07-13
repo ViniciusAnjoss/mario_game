b30ea3215f3bdb17ee142c7185f2e0fd20212a40
vinicius <vinicius.ssilvaa@outlook.com>
2023-07-13 10:40:08 -0300

    jogo do mario

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const scoreElement = document.querySelector('.score'); // Elemento HTML para exibir o placar
+let distance = 0; // Variável para armazenar a distância percorrida
+let gameLoop; // Variável para o intervalo do jogo
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const updateScore = () => {
+    scoreElement.textContent = distance; // Atualiza o valor do placar no elemento HTML
+}
+
+const startGame = () => {
+    gameLoop = setInterval(() => {
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+
+            mario.src = './images/game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '50px';
+
+            clearInterval(gameLoop);
+            alert('Game Over!');
+        }
+
+        distance++; // Incrementa a distância percorrida
+        updateScore(); // Atualiza o placar
+    }, 10);
+}
+
+document.addEventListener('keydown', jump);
+startGame();
